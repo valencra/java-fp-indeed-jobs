9b0b94bc65aaf526702163575fccb0eb9d4a9866
valencra <valencra@mcmaster.ca>
2018-01-14 09:18:33 -0500

    Implemented method for filtering for 3 junior jobs

--- src/main/java/com/teamtreehouse/jobs/App.java
+++ src/main/java/com/teamtreehouse/jobs/App.java
@@ -5,6 +5,7 @@ import com.teamtreehouse.jobs.service.JobService;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -24,10 +25,25 @@ public class App {
   }
 
   private static void explore(List<Job> jobs) {
-    // Your amazing code below...
+    // Portland, OR jobs
     jobs.stream()
         .filter(job -> job.getState().equals("OR"))
         .filter(job -> job.getCity().equals("Portland"))
         .forEach(System.out::println);
+
+    // 3 junior jobs
+    getThreeJuniorJobsStream(jobs).forEach(System.out::println);
+  }
+
+  private static List<Job> getThreeJuniorJobsStream(List<Job> jobs) {
+    return jobs.stream()
+        .filter(App::isJuniorJob)
+        .limit(3)
+        .collect(Collectors.toList());
+  }
+
+  private static boolean isJuniorJob(Job job) {
+    String title = job.getTitle().toLowerCase();
+    return title.contains("junior") || title.contains("jr");
   }
 }
